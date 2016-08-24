2c3be4b7cc2879763da4eb5be758aea9d1de9812
William Zhang <willzhang05@gmail.com>
2016-08-23 23:01:57 -0400

    Added jsoup lib

--- app/src/main/java/edu/tjhsst/ion/gcmFrame/ScheduleWidgetProvider.java
+++ app/src/main/java/edu/tjhsst/ion/gcmFrame/ScheduleWidgetProvider.java
@@ -8,11 +8,34 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 /**
  * Created by William Zhang on 8/14/2016.
  */
 public class ScheduleWidgetProvider extends AppWidgetProvider {
+    /*private String url = "https://ion.tjhsst.edu/schedule/embed/?date=2016-09-12";
     @Override
+    public void onEnabled(Context context) {
+
+    }*/
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // We can't trust the appWidgetIds param here, as we're using
         // ACTION_APPWIDGET_UPDATE to trigger our own updates, and
